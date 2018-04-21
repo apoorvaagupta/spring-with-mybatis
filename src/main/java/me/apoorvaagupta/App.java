ad6296fe4574a482da814add5a30f5c8358b2c8e
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-21 10:48:44 +0530

    driver code to test the handlers
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- src/main/java/me/apoorvaagupta/App.java
+++ src/main/java/me/apoorvaagupta/App.java
@@ -1,13 +1,23 @@
 package me.apoorvaagupta;
 
-/**
- * Hello world!
- *
- */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("me.apoorvaagupta")
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+
+        ProcessorHandler ph = context.getBean(ProcessorHandler.class);
+        String s = ph.process(1, "abd","cde");
+        System.out.println(s);
+
+        String s1 = ph.process(2, "abcjehckje");
+        System.out.println(s1);
     }
 }
