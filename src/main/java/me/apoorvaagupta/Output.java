7c89adb62ec81aad767a3a9ab45f2446076dc1ae
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-22 01:42:43 +0530

    created the pojo for output table
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ src/main/java/me/apoorvaagupta/Output.java
@@ -0,0 +1,30 @@
+package me.apoorvaagupta;
+
+public class Output {
+
+    private int id;
+    private int output;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOutput() {
+        return output;
+    }
+
+    public void setOutput(int output) {
+        this.output = output;
+    }
+
+    public Output(int id, int output) {
+
+        this.id = id;
+        this.output = output;
+    }
+
+}
