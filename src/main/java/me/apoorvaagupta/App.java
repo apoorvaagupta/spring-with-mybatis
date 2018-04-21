13f408fa5376db1f25c6b89f78d299d3e18319c9
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-22 01:45:32 +0530

    driver program to run the application
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- src/main/java/me/apoorvaagupta/App.java
+++ src/main/java/me/apoorvaagupta/App.java
@@ -1,23 +1,46 @@
 package me.apoorvaagupta;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 @Configuration
 @ComponentScan("me.apoorvaagupta")
+@MapperScan("me.apoorvaagupta")
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
+
+        Scanner scn = new Scanner(System.in);
+
         ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
 
+        InputHandler ih = context.getBean(InputHandler.class);
+        OutputHandler oh = context.getBean(OutputHandler.class);
         ProcessorHandler ph = context.getBean(ProcessorHandler.class);
-        String s = ph.process(1, "abd","cde");
-        System.out.println(s);
 
-        String s1 = ph.process(2, "abcjehckje");
-        System.out.println(s1);
+        System.out.println("What do you want to work with: 1. Integers\t2. Strings");
+        int type = scn.nextInt();
+
+        System.out.println("How do you want to take input: 1. Console\t2. File\t 3. Database");
+        int optionInput = scn.nextInt();
+
+        System.out.println("What process do you want to perform: 1. Addition\t2. Reversal");
+        int optionProcess = scn.nextInt();
+
+        System.out.println("How do you want to get output: 1. Console\t2. File\t 3. Database");
+        int optionOutput = scn.nextInt();
+
+        oh.writeOutput(optionOutput,ph.process(optionProcess, ih.takeInput(optionInput, type)));
+
     }
 }
